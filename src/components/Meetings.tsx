5ee6d70fc617786aed38e02f3114010d308b456c
Revati Tambe <24revati@gmail.com>
2024-07-08 21:18:00 -0700

    workshops page looks better

--- src/components/Meetings.tsx
+++ src/components/Meetings.tsx
@@ -2,9 +2,12 @@ import Workshop from "./Workshop"
 
 const Meetings = () => {
 	return (
-		<div className="p-8">
-			<h1 className="text-center pb-8">Meetings</h1>
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+		<div className="w-2/3 sm:w-3/4 md:w-2/3 lg:w-3/4 xl:w-4/5 m-auto">
+            <div className="text-center leading-loose py-8">
+    			<h1>Meetings</h1>
+                <p className="text-gray-400">Archived meeting slides and resources from our past meetings.</p>
+            </div>
+            <div className="m-auto grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 <Workshop number={1} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
                 <Workshop number={2} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
                 <Workshop number={3} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
--- src/components/Workshop.tsx
+++ src/components/Workshop.tsx
@@ -10,8 +10,8 @@ interface WorkshopProps {
 const Workshop: React.FC<WorkshopProps> = ({ title, number, date, slidesLink }) => {
 	return (
 		<a href={slidesLink} target='_blank' className='text-center lg:p-3 xl:p-4 border aspect-square border-gray-800 hover:border-teal-300 ease-linear duration-300 overflow-hidden flex flex-col justify-center items-center bg-slate-800 hover:bg-slate-800 bg-[linear-gradient(45deg,transparent_25%,rgba(68,68,68,.2)_50%,transparent_75%,transparent_100%)] rounded-xl bg-[length:250%_250%,100%_100%] bg-[position:-100%_0,0_0] bg-no-repeat transition-[background-position_0s_ease] hover:bg-[position:200%_0,0_0] hover:duration-[2500ms]'>
-			<p>{date}</p>
-			<h1 className='py-4'>GM #{number}:</h1>
+			<p className='text-gray-400'>{date}</p>
+			<h2>GM #{number}:</h2>
 			<p>{title}</p>
             <a className='underline text-teal-300' href={slidesLink}>Link to slides</a>
 		</a>
