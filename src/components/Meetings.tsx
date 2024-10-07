78935e283e2f3d18b0977989dc5d42ccae84e7f4
Revati Tambe <24revati@gmail.com>
2024-10-07 08:21:42 -0700

    updated worshops

--- src/components/Meetings.tsx
+++ src/components/Meetings.tsx
@@ -8,13 +8,13 @@ const Meetings = () => {
                 <p className="text-gray-400">Archived meeting slides and resources from our past meetings.</p>
             </div>
             <div className="m-auto grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                <Workshop number={1} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
-                <Workshop number={2} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
-                <Workshop number={3} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
+                <Workshop number={0} title={"Introduction"} date={"9/30/2024"} slidesLink={"https://docs.google.com/presentation/d/1fS4N5aenI20SVkA37hRDzX07dZIvPwiweCKMFBN2qJE/edit?usp=sharing"} />
+                <Workshop number={1} title={"Intro to Python"} date={"10/07/2024"} slidesLink={"https://docs.google.com/presentation/d/10ecaNvc0aUz-Ix-USUChPYEZCRGUX8KMSPHj-JP7EJU/edit?usp=sharing"} />
+                {/* <Workshop number={2} title={"Malware & Keyloggers"} date={"10/14/2024"} slidesLink={"https://docs.google.com/presentation/d/1-ng-aoJTp1N4T78SV0vSOkhRv5tzx-i-HBEs8iIO5wA/edit?usp=sharing"} /> */}
+                {/* <Workshop number={3} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
                 <Workshop number={4} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
                 <Workshop number={5} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
-                <Workshop number={6} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
-                <Workshop number={7} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} />
+                <Workshop number={6} title={"Sample Workshop Title"} date={"08/19/2024"} slidesLink={""} /> */}
             </div>
 		</div>
 	)
