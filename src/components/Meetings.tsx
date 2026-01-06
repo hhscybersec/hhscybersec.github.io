ddc6a11dc6fe65de9d133819ffc1cf5febda12f2
Revati Tambe <24revati@gmail.com>
2026-01-05 19:38:55 -0800

    nextjs

--- src/components/Meetings.tsx
+++ src/components/Meetings.tsx
@@ -10,7 +10,7 @@ const Meetings = () => {
             <div className="m-auto grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 <Workshop number={0} title={"Year-Long Plans"} date={"9/22/2025"} slidesLink={"https://docs.google.com/presentation/d/1mPUCGvNj3imbnlqK48rJp0ThTUekardiRkKMd1szJRc/edit?usp=sharing"} />
                 <Workshop number={1} title={"Command Line Basics"} date={"9/29/2025"} slidesLink={"https://docs.google.com/presentation/d/1w8KxUjFBx0kUfe1OdhKoY9yKwyOyoqIPFsntwTyviOo/edit?usp=sharing"} />
-                <Workshop number={2} title={"Intro to PicoCTF"} date={"10/05/2025"} slidesLink={"https://docs.google.com/presentation/d/19B0srNeZ3cj8mKtUtxOyxIyiqFiZpsBL1-deafv-ZSs/edit?usp=sharing"} />
+                <Workshop number={2} title={"Intro to PicoCTF"} date={"10/05/2025."} slidesLink={"https://docs.google.com/presentation/d/19B0srNeZ3cj8mKtUtxOyxIyiqFiZpsBL1-deafv-ZSs/edit?usp=sharing"} />
             </div>
 		</div>
 	)
