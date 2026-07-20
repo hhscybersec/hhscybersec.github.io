d41a026846c7a33b49c86efc436c7b3704703845
Revati Tambe <24revati@gmail.com>
2026-07-20 16:42:07 -0700

    updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
 			<div className="w-fit m-auto">
 				<h2 className='text-center pb-6'>Officers</h2>
 				<div className='pb-8 w-4/5 sm:w-3/4 md:w-2/3 lg:w-3/4 xl:w-4/5 m-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
-					<Officer name="Barack Obama" position="President" picture="/officers/placeholder.webp"/>
+					<Officer name="Elad Korman" position="President" picture="/officers/placeholder.webp"/>
 					<Officer name="Aaditya Shinde" position="Vice President" picture="/officers/placeholder.webp"/>
                     <Officer name="Debargha Sarkar" position="Secretary/Treasurer" picture="/officers/placeholder.webp"/>
 				</div>
